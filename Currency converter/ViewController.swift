7138d418b707f415ddc744dc96eee4d8bf57449d
Stavratiy Andriy <stavraty@gmail.com>
2023-05-10 13:07:16 +0300

    header constraints

--- Currency converter/ViewController.swift
+++ Currency converter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var headerBackgroundView: UIView!
+    @IBOutlet weak var firstBackgroundView: UIView!
+    @IBOutlet weak var secondBackgroundView: UIView!
+    @IBOutlet weak var thirdBackgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundViewSettings()
+    }
+    func backgroundViewSettings() {
+        firstBackgroundView.layer.cornerRadius = firstBackgroundView.frame.height / 2
+        secondBackgroundView.layer.cornerRadius = secondBackgroundView.frame.height / 2
+        thirdBackgroundView.layer.cornerRadius = thirdBackgroundView.frame.height / 2
     }
 
-
 }
-
